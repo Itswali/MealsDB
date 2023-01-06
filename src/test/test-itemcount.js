c3cd989d1dfaad96bda0cd519d919ab8909d9d2d
Utsargo Roy <royutsargo@gmail.com>
2023-01-06 19:25:18 +0600

    Add test for item count

---
+++ src/test/test-itemcount.js
@@ -0,0 +1,10 @@
+const mealsCountTest = (arr) => {
+  let result = 0;
+  if (arr.length === undefined) {
+    result = 0;
+  } else {
+    result = arr.length;
+  }
+  return result;
+};
+export default mealsCountTest;
