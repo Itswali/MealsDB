cc09210e4f71d408b576c5ae532ed725ca13c5bf
Utsargo Roy <royutsargo@gmail.com>
2023-01-06 19:36:44 +0600

    Fix eslint error

--- src/test/item.test.js
+++ src/test/item.test.js
@@ -2,38 +2,38 @@
  * @jest-environment jsdom
  */
 
-import mealsCountTest from "./test-itemcount.js";
+import mealsCountTest from './test-itemcount.js';
 
 const mockData = [
   {
-    strMeal: "Baked salmon with fennel & tomatoes",
-    strMealThumb: "https://xyz.xyz",
-    idMeal: "52959",
+    strMeal: 'Baked salmon with fennel & tomatoes',
+    strMealThumb: 'https://xyz.xyz',
+    idMeal: '52959',
   },
   {
-    strMeal: "Baked salmon with fennel & tomatoes",
-    strMealThumb: "https://xyz.xyz",
-    idMeal: "52960",
+    strMeal: 'Baked salmon with fennel & tomatoes',
+    strMealThumb: 'https://xyz.xyz',
+    idMeal: '52960',
   },
   {
-    strMeal: "Baked salmon with fennel & tomatoes",
-    strMealThumb: "https://xyz.xyz",
-    idMeal: "52961",
+    strMeal: 'Baked salmon with fennel & tomatoes',
+    strMealThumb: 'https://xyz.xyz',
+    idMeal: '52961',
   },
   {
-    strMeal: "Baked salmon with fennel & tomatoes",
-    strMealThumb: "https://xyz.xyz",
-    idMeal: "52962",
+    strMeal: 'Baked salmon with fennel & tomatoes',
+    strMealThumb: 'https://xyz.xyz',
+    idMeal: '52962',
   },
   {
-    strMeal: "Baked salmon with fennel & tomatoes",
-    strMealThumb: "https://xyz.xyz",
-    idMeal: "52963",
+    strMeal: 'Baked salmon with fennel & tomatoes',
+    strMealThumb: 'https://xyz.xyz',
+    idMeal: '52963',
   },
 ];
 
-describe("We are", () => {
-  test("testing commentCount function", () => {
+describe('We are', () => {
+  test('testing commentCount function', () => {
     expect(mealsCountTest(mockData)).toBe(5);
   });
 });
